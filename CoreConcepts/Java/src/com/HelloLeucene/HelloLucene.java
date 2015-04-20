fe13afc826b6605bc2170589edc364747bb3b8e9
unknown <vsach3@delvsach343217.sapient.com>
2015-04-20 03:28:43 +0100

    Updated example with further comments.

--- CoreConcepts/Java/src/com/HelloLeucene/HelloLucene.java
+++ CoreConcepts/Java/src/com/HelloLeucene/HelloLucene.java
@@ -1,22 +1,30 @@
 package com.HelloLeucene;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
 import org.apache.lucene.index.DirectoryReader;
+
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
+
 import org.apache.lucene.index.IndexWriterConfig;
+
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
+
 import java.io.IOException;
 
 public class HelloLucene {
@@ -25,26 +33,28 @@ public class HelloLucene {
     //    The same analyzer should be used for indexing and searching
     StandardAnalyzer analyzer = new StandardAnalyzer();
 
-    // 1. create the index
+    // 1. Create the in memory index
     Directory index = new RAMDirectory();
 
+    // use analyzer in index config
     IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
+    // Create writer to in-memory index with above config 
     IndexWriter w = new IndexWriter(index, config);
+    
     addDoc(w, "Lucene in Action", "193398817");
     addDoc(w, "Lucene for Dummies", "55320055Z");
     addDoc(w, "Managing Gigabytes", "55063554A");
     addDoc(w, "The Art of Computer Science", "9900333X");
     w.close();
 
-    // 2. query
+    // 2. Lucene query string 
     String querystr = args.length > 0 ? args[0] : "lucene";
 
-    // the "title" arg specifies the default field to use
-    // when no field is explicitly specified in the query.
+    // "title" arg specifies default field to use when no field 
+    // is explicitly specified in the query.
     Query q = new QueryParser("title", analyzer).parse(querystr);
 
-    // 3. search
+    // 3. search - open index, search index, TopScoreDocCollect, 
     int hitsPerPage = 10;
     IndexReader reader = DirectoryReader.open(index);
     IndexSearcher searcher = new IndexSearcher(reader);
